6091966e3421d1288affd2fe03b21a61329dcf47
JoshPham <JTPham911@gmail.com>
2024-02-21 03:10:08 +0000

    made basic Superhuman Superhero and Supervillain classes, not complete yet though

---
+++ Main.java
@@ -0,0 +1,6 @@
+package csa.Superhuman;
+
+public class Main {
+    public static void main(String[] args) {
+    }
+}
---
+++ Universe.java
@@ -0,0 +1,11 @@
+package csa.Superhuman;
+
+public class Universe {
+    private Superhero hero;
+    private Supervillain villain;
+
+    public Universe(Superhero hero, Supervillain villain) {
+        this.hero = hero;
+        this.villain = villain;
+    }
+}
